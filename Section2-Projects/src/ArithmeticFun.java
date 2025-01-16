6d8139834270ac9a70009ef2f632079152ee9091
Ademar_Kumabe <ademardevti@gmail.com>
2025-01-16 01:11:56 -0300

    Fundamentos Java Curso Oreilly

---
+++ Section2-Projects/src/ArithmeticFun.java
@@ -0,0 +1,36 @@
+public class ArithmeticFun {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+
+        int result = a +b;
+        int difference = a -b;
+        int product = a * b;
+        int quotient = b / a;
+        int remainder = b % a;
+
+        System.out.println("result is " + result);
+        System.out.println("diff is " + difference);
+        System.out.println("product is " + product);
+        System.out.println("quotient is "+ quotient);
+        System.out.println("remainder is "+ remainder);
+
+        result += 20;
+        System.out.println("result is now " + result);
+
+        result++;
+        System.out.println("result++ " + result);
+
+        result--;
+        System.out.println("result-- " + result);
+
+        //Desafio
+
+        int g = 2;
+        int h = 4;
+
+        int resultado = g*h;
+        resultado *= 2;
+        System.out.println("Resultado da multiplicação de um produto por dois, na mesma variavel: " + resultado);
+    }
+}
---
+++ Section2-Projects/src/VariableFun.java
@@ -0,0 +1,14 @@
+public class VariableFun {
+    public static void main(String[] args) {
+        int myAge; //declaration with camel case standart
+        myAge = 32; //initialization
+        String name = "Ademar";
+
+        final int SOME_NUM = 150;
+
+        String myHomeTown = "São Paulo";//tarefa adcionar variável para sua cidade natal
+
+        System.out.println(name + " is " +myAge + " and living in " +myHomeTown); //se usarmos o print ao inves de println ele não
+    }
+}
+
